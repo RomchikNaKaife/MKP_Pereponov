231e69243d4f26b857e2fc2547f95cdb47f380e6
Roman <roman.spam2004@yaandex.ru>
2023-12-24 17:13:43 +0300

    Initial Commit

---
+++ MKP_Pereponov/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MKP_Pereponov
+//
+//  Created by Роман Перепонов on 24.12.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
